afa0ec601312603b693d417c09e789aa49784f7f
v-peinado <peinado1985@gmail.com>
2023-08-25 17:26:47 +0200

    [ADD] First commit

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpeinado <vpeinado@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 15:06:54 by vpeinado          #+#    #+#             */
+/*   Updated: 2022/12/03 17:43:27 by vpeinado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nitems, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(nitems * size);
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	ft_bzero(ptr, (nitems * size));
+	return (ptr);
+}
